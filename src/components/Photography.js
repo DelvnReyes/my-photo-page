fb1343827e29974143c9eca151aa66ac3bd684c9
Delvin Reyes <DelvinReyes.95@Gmail.com>
2024-10-21 22:00:40 -0400

    testing functionality

--- src/components/Photography.js
+++ src/components/Photography.js
@@ -7,10 +7,10 @@ const Photography = ({ image, onClick }) => {
       <img src={image.photo} alt="photoshoots" className="w-full"></img>
       <div className="px-6 py-2">
         <ul>
-          <li className="flex">
+          {/* <li className="flex">
             <h2 className="px-1 text-lg text-white">{image.social}</h2>
             <h2 className="px-1 text-white">{image.name}</h2>
-          </li>
+          </li> */}
 
           {/* <li>
             <strong className="px-1 text-white">Views: {image.views}</strong>
