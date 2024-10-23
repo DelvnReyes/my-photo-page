89f3b55fdb083da91fdf24eaafc72d3a852b9714
Delvin Reyes <DelvinReyes.95@Gmail.com>
2024-10-22 20:45:39 -0400

    revamped site with footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="flex items-center justify-center text-white p-4 gap-2 ">
       <div className="text-[0.9rem] text-blue">
-        &#169; 2024 All rights deserved.
+        &#169; 2024, LLC. All Rights Reserved.
       </div>
     </div>
   );
